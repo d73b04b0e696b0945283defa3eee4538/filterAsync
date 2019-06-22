1fdfaa496b7b5d1223312051475284e2116f6206
ganesh <null>
2019-06-22 19:47:51 +0530

    Add example

---
+++ example/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Module
+const filterAsync = require("../filterAsync.js");
+const games = ["Chess", "Cricket", "Soccer", "Tennis"];
+const test = (sport) => ["Chess", "Tennis", "Cricket"].includes(sport);
+
+!async function(){
+	console.log(await filterAsync(games, test));
+	console.log(await filterAsync(games, test, {first: 2}));
+	console.log(await filterAsync(games, test, {last: 2}));
+}()
